f132c7a5cd4cb5af23c387fb7b68631188aa610c
Alexis28J <jordy2806@hotmail.com>
2026-02-18 09:02:24 +0100

    Manca step 6

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../../hero';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,8 +13,28 @@ import { Hero } from '../../hero';
 })
 
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+  
+  hero: Hero | undefined;
 
-  @Input() hero?: Hero;
+  constructor(
+  private route: ActivatedRoute,
+  private heroService: HeroService,
+  private location: Location
+) {}
+
+ngOnInit(): void {
+  this.getHero();
+}
+
+getHero(): void {
+  const id = Number(this.route.snapshot.paramMap.get('id'));
+  this.heroService.getHero(id)
+    .subscribe(hero => this.hero = hero);
+}
+
+goBack(): void {
+  this.location.back();
+}
 
 }
